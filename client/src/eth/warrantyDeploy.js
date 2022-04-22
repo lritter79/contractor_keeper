3c5982985b49c3a0d013b034fb7964f1f00d8976
Levon Ritter <lritter@automated-health.com>
2022-04-22 15:05:00 -0400

    added hook to store address to local storage

--- client/src/eth/warrantyDeploy.js
+++ client/src/eth/warrantyDeploy.js
@@ -1,12 +1,12 @@
 import { web3 } from "./web3";
 import { getWarranty } from "./getWarranty";
-import warrantyByteCode from "./warrantyAbi";
+import warrantyByteCode from "./warrantyByteCode";
 
 export async function deployWarranty(warrantyArguments) {
     let warrantyContract = getWarranty();
-    let gas = warrantyContract.deploy({data:warrantyByteCode, arguments: warrantyArguments}).estimateGas((err, g) => console.log(g));
+    let gas = await warrantyContract.deploy({data:warrantyByteCode, arguments: warrantyArguments}).estimateGas((err, g) => console.log(g));
     const accounts = await web3.eth.getAccounts();
-    let params = {gas: gas, address: accounts[0]}
+    let params = {gas: gas, from: accounts[0]}
     return await warrantyContract.deploy({data:warrantyByteCode, arguments: warrantyArguments}).send(params)
     .then(function(newContractInstance){
         return newContractInstance.options.address // instance with the new contract address
--- client/src/routes/WarrantyScreen/WarrantyScreen.js
+++ client/src/routes/WarrantyScreen/WarrantyScreen.js
@@ -7,10 +7,17 @@ import ProviderForm from "./Forms/ProviderForm"
 import WarrantyDetailsForm from "./Forms/WarrantyDetailsForm";
 import { useStepsForm } from 'sunflower-antd';
 import { deployWarranty } from "../../eth/warrantyDeploy";
+import useLocalStorage from "../../customHooks/useLocalStorage";
+
 
 const WarrantyScreen = () => {
     const { Step } = Steps;
-    const [address, setAddress] = useState(null)
+    const addressPlaceHolder = Object.freeze({
+        HOLDER_ADDRESS: '0x630aB4E818Bd0c1ADAF690aDC3703A49D6D7f07E',
+        PROVIDER_ADDRESS: '0x630aB4E818Bd0c1ADAF690aDC3703A49D6D7f07E',
+        TRANSFER_ADDRESS: '0x630aB4E818Bd0c1ADAF690aDC3703A49D6D7f07E'
+    });
+    const [address, setAddress] = useLocalStorage("contractAddress", null)
     const {
         form,
         current,
@@ -22,12 +29,9 @@ const WarrantyScreen = () => {
       } = useStepsForm({
         async submit (values) {
           console.log(values);
-          let arr = [];
-          Object.keys(values).forEach(key => {
-            let value = values[key];
-            arr.push(value);
-          });
-          await deployWarranty(arr)
+          let arr = shapeData(values);
+          let address = await deployWarranty(arr)
+          setAddress(address)
           //await new Promise(r => setTimeout(r, 1000));
           return 'ok';
         },
@@ -49,6 +53,71 @@ const WarrantyScreen = () => {
             See your contract at {address}
         </div>
     ]
+
+    function shapeData(formOutput) {
+        /* let params = [];
+        params.push(formOutput.expirationDate.unix());// uint _warrantyExpirationDate, //date in seconds past epoch
+        params.push(formOutput.warrantyExpires);// bool _warrantyExpires,
+        params.push(formOutput.daysToCorrect);// uint _numberOfDaysToCorrect,
+        params.push(formOutput.dateOfWorkCompletion.unix());// uint _workCompleted, //date in seconds past epoch
+        params.push(formOutput.projectName);// string memory _projectName,
+        params.push(formOutput.descriptionOfProject);// string memory _description,
+        params.push(formOutput.isTransferable);// bool _transferable,
+        params.push(formOutput.provider);// Party memory _warrantyProvider,
+        params.push(formOutput.holder);// Party memory _warrantyHolder,
+        params.push(formOutput.useHolderAddress ? formOutput.holder.location : formOutput.location);// Location memory _location */
+        let params =[
+            1648551148,      //uint _warrantyExpirationDate, //date in seconds past epoch
+            true, //_warrantyExpires
+            3,        //uint _numberOfDaysToCorrect,
+            1648501148,        //uint _workCompleted, //date in seconds past epoch
+            "Test",        //string memory _projectName,
+            "A test",        //string memory _description,
+            true,       //bool _transferable
+            {
+                name: "The Stine Family", 
+                partyAddress: addressPlaceHolder.PROVIDER_ADDRESS,
+                physicalLocation: {
+                    streetAddressLine1:'123 Fake Street',
+                streetAddressLine2:'',
+                city: 'PitBurgh',
+                state: 'PA',
+                postalCode: "77777"
+                },
+                contactInfo:{
+                    email: "fakeemail@gmail.com",
+                    phoneMobile: "4022931029",
+                    phoneHome:""
+        
+                } 
+            },      //Party memory _warrantyProvider,
+            {
+                name: "The Stine Contracting Co.", 
+                partyAddress: addressPlaceHolder.HOLDER_ADDRESS,
+                physicalLocation: {
+                    streetAddressLine1:'123 Fake Street',
+                streetAddressLine2:'',
+                city: 'PitBurgh',
+                state: 'PA',
+                postalCode: "77777"
+                },
+                contactInfo:{
+                    email: "fakeemail@gmail.com",
+                    phoneMobile: "4022931029",
+                    phoneHome:""
+        
+                } 
+            },        //Party memory _warrantyHolder,
+            {
+                streetAddressLine1:'123 Fake Street',
+                streetAddressLine2:'',
+                city: 'PitBurgh',
+                state: 'PA',
+                postalCode: "77777"
+            },        //Location memory _location
+        ]
+        return params;
+    }
     
     const next = () => {
         gotoStep(current + 1)
